cb6efabd5868102170eae5afd561ae7d95b39ea9
mygy <demouser2@cloudlockdemo.com>
2016-06-27 15:14:07 -0400

    Update auth.py

--- auth.py
+++ auth.py
@@ -11,7 +11,5 @@ class blah (object):
         self.token = token
         self.base_url = base_url
         self.session = session = requests.session()
-        retries = Retry(total=100, status_forcelist=(429, 500, 502, 504), backoff_factor=0.1)
-        session.mount(self.base_url, requests.adapters.HTTPAdapter(max_retries=retries))
         session.headers.update(
             {'Content-Type': 'application/json', 'Authorization': 'Bearer {}'.format(self.token)})
